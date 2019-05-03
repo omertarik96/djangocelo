2f447c16f55fb903fb7637f042a5a3e4a78a541b
Omer Tarik Koc <omertarikkoc@gmail.com>
2019-05-02 21:50:53 -0500

    bootstrap 4 and custom theme added

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Model for each course in the application
+# This models consist of Subject, Course and Module
+
+
+class Subject(models.Model):
+    """ This model will implement subject of the courses """
+    title = models.CharField(max_length=200) # Title of the subject, max_length 200
+    slug = models.SlugField(max_length=200, unique=True) # Slug(url) of the subject, this field wil be unique
+
+    class Meta:
+        ordering = ['title'] # Default ordering for the subject model is by title
+
+    def __str__(self):
+        """Default return value for this model. This will be used in the admin panel and several different places"""
+        return self.title
+
--- djangocelo/urls.py
+++ djangocelo/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import home_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', home_view, name="home")
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
